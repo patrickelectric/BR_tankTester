aee3970c622a1523fe673f29a99221515ab38ea4
Patrick José Pereira <patrickelectric@gmail.com>
2019-03-05 08:27:16 -0800

    tanktester: Update serial object names
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- tanktester.py
+++ tanktester.py
@@ -37,21 +37,25 @@ time.sleep(5)
 
 # Power supply output
 # scale rs232 to usb
-ser = serial.Serial(#scale rs232 to usb
+power_supply_serial = serial.Serial(
 	    port='/dev/ttyUSB0',\
 	    baudrate=19200,\
 	    parity=serial.PARITY_NONE,\
 	    stopbits=serial.STOPBITS_ONE,\
 	    bytesize=serial.EIGHTBITS,\
 	    timeout=3)
-ser2 = serial.Serial(#Arduino used as ADC
+
+# Force sensor
+force_sensor_serial = serial.Serial(
 	    port='/dev/ttyACM0',\
 	    baudrate=19200,\
 	    parity=serial.PARITY_NONE,\
 	    stopbits=serial.STOPBITS_ONE,\
 	    bytesize=serial.EIGHTBITS,\
 	    timeout=3)
-ser3 = serial.Serial(#Arduino used as RPM sensor
+
+# RPM sensor
+rpm_sensor_serial = serial.Serial(
 	    port='/dev/ttyACM1',\
             baudrate=19200,\
             parity=serial.PARITY_NONE,\
@@ -61,10 +65,10 @@ ser3 = serial.Serial(#Arduino used as RPM sensor
 print "Welcome to Tankinator test platform!! RPM sensing added in 10/2018 by Adam Simko"
 print "This script automates tank testing by running the thruster for a set durationm while recording data, with a long duration pause between each throttle level for tank to settle"
 print "Push ctrl+c to exit at any time. Launch script with /tankinator/./tanktester.py"
-ser2.flushInput()# clear input before taking reading for rough sync to seperate Arduino :(
-ser3.flushInput()# same ^^
-scale_level = (ser2.readline()) # Arduino configured to output at 10hz of this script
-scale_level = (ser3.readline()) #same^^
+force_sensor_serial.flushInput()# clear input before taking reading for rough sync to seperate Arduino :(
+rpm_sensor_serial.flushInput()# same ^^
+scale_level = (force_sensor_serial.readline()) # Arduino configured to output at 10hz of this script
+scale_level = (rpm_sensor_serial.readline()) #same^^
 print "The current reading of the force sensor is %s"%scale_level
 test_name = raw_input('Enter name of this test: ')
 timenow='{date:%Y.%m.%d %H.%M.%S}.csv'.format( date=datetime.datetime.now() )
@@ -108,25 +112,25 @@ while (throttle <=MAX_THROTTLE) and (prevthrottle != MAX_THROTTLE): # exit when 
 		time.sleep(1)# let things settle out before starting log	
 		runmotor = 0 #keep it from increasing again within same period
 	try:  #to talk to power supply
-		ser.write('MEASURE:VOLTAGE:DC?') #from documentation. 
-		ser.write(b'\rL1\r') #sends enter after request to get return
-		VOLTAGE = float(ser.readline()) #cast to float
-		ser.write('MEASURE:CURRENT:DC?')
-		ser.write(b'\rL1\r')
-		CURRENT = float(ser.readline())
+		power_supply_serial.write('MEASURE:VOLTAGE:DC?') #from documentation.
+		power_supply_serial.write(b'\rL1\r') #sends enter after request to get return
+		VOLTAGE = float(power_supply_serial.readline()) #cast to float
+		power_supply_serial.write('MEASURE:CURRENT:DC?')
+		power_supply_serial.write(b'\rL1\r')
+		CURRENT = float(power_supply_serial.readline())
 		POWER = VOLTAGE * CURRENT #calculate the useful metric
 	except:
 		print "power supply comm error dude"
 	try: #to read the external ADC. Need to update prompts to include lever arm, remove this from Arduino sketch
-		ser2.flushInput()# clear input before taking reading for rough sync to seperate Arduino :(
-		input = (ser2.readline()) # Arduino configured to output at 10hz of this script
+		force_sensor_serial.flushInput()# clear input before taking reading for rough sync to seperate Arduino :(
+		input = (force_sensor_serial.readline()) # Arduino configured to output at 10hz of this script
 		FORCE = float(input)/(MECH_VERTICAL/MECH_HORIZ)#add scaling here to include lever arm input, remove from Arduino output
 		efficiency = float(((FORCE*float(453.59))/POWER))
 	except:
                         print "sorry man, force sensor com error"
 	try: #to read the external RPM sensor.
-		ser3.flushInput()# clear input before taking reading for rough sync to seperate Arduino :(
-		input = (ser3.readline()) # Arduino configured to output at 10hz of this script
+		rpm_sensor_serial.flushInput()# clear input before taking reading for rough sync to seperate Arduino :(
+		input = (rpm_sensor_serial.readline()) # Arduino configured to output at 10hz of this script
 		RPM = 60000000/(float(input)*(POLE_COUNT/2))#Get RPM from filtered period value, incorporate pole count
 		print {"efficiency":efficiency,"PWM":throttle,"RPM":RPM,"CURRENT":CURRENT,"VOLTAGE":VOLTAGE,"POWER":POWER,"FORCE":FORCE,"TIME":datetime.datetime.now().strftime("%H:%M:%S.%f")} #attempt at json readable format
 	except:
